b23d4b339813ddbb5377fc26a803b0744213dd6e
Darius Goore <dariusgoore@dariuss-mbp.myfiosgateway.com>
2022-06-26 08:15:56 -0400

    adding dotenv and configure Sequelize

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
 var corsOptions = {
     origin: "http://localhost:8081"
@@ -11,6 +13,14 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 // simple route
+
+const db = require("./app/models");
+const Role = db.role;
+db.sequelize.sync({force: true}).then(() => {
+    console.log('Drop and Resync Db');
+    initial();
+});
+
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to the Music Library application." });
 });
@@ -19,3 +29,20 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
+
+function initial() {
+    Role.create({
+        id: 1,
+        name: "user"
+    });
+
+    Role.create({
+        id: 2,
+        name: "moderator"
+    });
+
+    Role.create({
+        id: 3,
+        name: "admin"
+    });
+}
